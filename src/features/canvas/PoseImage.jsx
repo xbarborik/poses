92b8e3503fe414e7f679eb4d9fa44d26bc97ee37
xbarborik <martinbarborik9@gmail.com>
2023-11-20 22:13:28 +0100

    Changed yoga to images in public

--- src/features/canvas/PoseImage.jsx
+++ src/features/canvas/PoseImage.jsx
@@ -2,7 +2,7 @@ import { Image } from "react-konva";
 import useImage from "use-image";
 
 function PoseImage() {
-  const [image] = useImage("yoga/warrior.png");
+  const [image] = useImage("./images/warrior.png");
   return <Image image={image} />;
 }
 
