ce5b0d62ba947de102280a2411e8c64758e3022a
xbarborik <martinbarborik9@gmail.com>
2023-11-21 00:36:19 +0100

    Using import in useImage

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       </TopBar>
       <Main>
         <img src={warrior} />
+        <img src={"/images/warrior.png"} />
         <Canvas />
         <Toolbar />
       </Main>
--- src/features/canvas/PoseImage.jsx
+++ src/features/canvas/PoseImage.jsx
@@ -1,8 +1,10 @@
 import { Image } from "react-konva";
 import useImage from "use-image";
 
+import warrior from "/images/warrior.png";
+
 function PoseImage() {
-  const [image] = useImage("../images/warrior.png");
+  const [image] = useImage(warrior);
   return <Image image={image} />;
 }
 
