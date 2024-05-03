03bb4c0200e254a417e7063c9441478df1ac3799
xbarborik <martinbarborik9@gmail.com>
2024-05-04 01:18:50 +0200

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,34 +1,32 @@
-const BASE = "/poses";
-
-// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/How_to/Share_data_between_apps
 self.addEventListener("fetch", (event) => {
-  console.log("Fetch event:", event);
   const requestPath = new URL(event.request.url).pathname;
-  console.log("Request path:", requestPath);
-  console.log("Request method:", event.request.method);
 
-  if (event.request.method === "POST" && requestPath === `${BASE}/share`) {
+  if (event.request.method === "POST" && requestPath === "/poses/share") {
     event.respondWith(
       (async () => {
         try {
           const formData = await event.request.formData();
-          console.log("Form data:", formData);
-          const imageFile = formData.get("images");
-          const imageUrl = imageFile ? URL.createObjectURL(imageFile) : null;
-          console.log("Image URL:", imageUrl);
+          const imageFile = formData.get("images"); // Use the correct key name
 
-          const client = await self.clients.get(
-            event.resultingClientId || event.clientId
-          );
-          console.log("Client:", client);
+          if (imageFile) {
+            const reader = new FileReader();
 
-          if (client && imageUrl) {
-            client.postMessage({ imageUrl });
-            console.log("Posted message to client with imageUrl");
+            // Read the blob as an ArrayBuffer
+            const arrayBuffer = await imageFile.arrayBuffer();
+
+            // Convert ArrayBuffer to Blob
+            const blob = new Blob([arrayBuffer], { type: imageFile.type });
+
+            const client = await self.clients.get(
+              event.resultingClientId || event.clientId
+            );
+
+            if (client) {
+              client.postMessage({ blob });
+            }
           }
 
-          console.log("Redirecting to", `${BASE}/#/share`);
-          return Response.redirect(`${BASE}/#/share`, 303);
+          return Response.redirect("/poses/#/share", 303);
         } catch (error) {
           console.error("Error in fetch handler:", error);
           return new Response("Error processing request", { status: 500 });
